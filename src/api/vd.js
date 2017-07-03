1fee912e47d937c312164156f22cd6f829a92ca0
chengwm <chengwm@gmail.com>
2017-07-03 12:23:39 +0800

    Add first db call endpoint

--- src/api/vd.js
+++ src/api/vd.js
@@ -1,3 +1,6 @@
+const Joi = require("joi")
+const _ = require('lodash')
+
 exports.register = function (server, options, next) {
   server.route({
     method: "GET",
@@ -6,6 +9,47 @@ exports.register = function (server, options, next) {
       reply("Test worked")
     }
   })
+
+  // Get value given a key
+  server.route({
+    method: "GET",
+    path: "/object/{key}",
+    config: {
+      validate: {
+        params: {
+          key: Joi.string().required()
+        }
+      }
+    }, 
+    handler: function(request, reply){
+      let m = getModels(request)
+      let key = request.params.key
+
+      m.VDModel.findOne({
+        attributes: ['value'],
+        where: { key },
+        order: [['createdAt', 'DESC']]
+      }).then(result => {
+        if(result){
+          reply(result.toJSON())
+        } else {
+          reply("No results found")
+        }
+      }).catch(err => reply(err))
+    }
+  })
+
+
+function getDB(request){
+  return request.server.plugins["dbSetup"].db
+}
+
+function getModels(request){
+  return request.server.plugins["dbSetup"].models
+}
+
+
+
   next()
 }
 
